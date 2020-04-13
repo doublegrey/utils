a30684fb4ab217cbdbcbe0171ad50d8b5d3e9d35
Sergey Kropinov <sergejkropinov@gmail.com>
2020-04-13 15:29:00 +0300

    Read flow configuration

---
+++ config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Instances map[string]instance
+	Producers map[string]producer
+	Consumers map[string]consumer
+}
+type instance struct {
+	ID      string
+	Listen  string
+	Produce string
+}
+type producer struct {
+	ID      string
+	Listen  string
+	Produce string
+	Topic   string
+	Address string
+}
+type consumer struct {
+	ID      string
+	Produce string
+	Topic   string
+	Address string
+}
+
+func (c config) Read(path string) error {
+	if _, err := toml.DecodeFile(path, &c); err != nil {
+		return err
+	}
+}
