44d0e76145c788af17f473fe0ae9b9f6ea8c80b2
Sergey Kropinov <sergejkropinov@gmail.com>
2020-05-12 20:24:24 +0300

    ConnectSocket() now supports deadline timeout

--- socket.go
+++ socket.go
@@ -1,28 +1,58 @@
 package utils
 
 import (
+	"context"
 	"net"
 	"os"
 	"syscall"
 	"time"
 )
 
-// sock, err := uds.ConnectSocket("/env/example.sock", time.Second*2)
+// sock, err := uds.ConnectSocket("/env/example.sock", time.Second*1, time.Second*5)
 //
 // sock.Write(msg)
-func ConnectSocket(address string, delay time.Duration) (*net.UnixConn, error) {
-	for {
-		addr, err := net.ResolveUnixAddr("unix", address)
-		if err != nil {
-			return nil, err
-		}
-		conn, err := net.DialUnix("unix", nil, addr)
-		if err != nil { //FIXME
-			time.Sleep(delay)
-			continue
-		}
-		return conn, nil
+func ConnectSocket(address string, interval, deadline time.Duration) (*net.UnixConn, error) {
+	type Result struct {
+		conn *net.UnixConn
+		err  error
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), deadline)
+	defer cancel()
+	done := make(chan Result, 1)
+	go func(done chan<- Result) {
+		wait := func() bool {
+			select {
+			case <-time.After(interval):
+				return true
+			case <-ctx.Done():
+				// Canceled, don't continue
+				done <- Result{err: ctx.Err()}
+				return false
+			}
+		}
+		// Try connecting until either connected or canceled
+		for {
+			addr, err := net.ResolveUnixAddr("unix", address)
+			if err != nil {
+				if !wait() {
+					return
+				}
+				continue
+			}
+			conn, err := net.DialUnix("unix", nil, addr)
+			if err == nil {
+				if !wait() {
+					return
+				}
+				continue
+			}
+			// Connection established successfully
+			done <- Result{conn: conn}
+			break
+		}
+	}(done)
+	r := <-done
+	return r.conn, r.err
 }
 
 // listener, err := uds.OpenSocket("/env/example.sock")
